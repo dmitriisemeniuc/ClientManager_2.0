f6e61a48ba60ead5c3b306b33e4945b07a78c879
Dmitrii Semeniuc <dmitriisemeniuc@gmail.com>
2016-11-20 13:56:12 +0200

    Login improvement (#2)

--- app/src/main/java/com/semeniuc/dmitrii/clientmanager/login/LoginView.java
+++ app/src/main/java/com/semeniuc/dmitrii/clientmanager/login/LoginView.java
@@ -1,10 +1,9 @@
 package com.semeniuc.dmitrii.clientmanager.login;
 
-import com.google.android.gms.auth.api.signin.GoogleSignInResult;
-import com.google.android.gms.common.api.OptionalPendingResult;
 import com.semeniuc.dmitrii.clientmanager.BaseView;
 
 public interface LoginView extends BaseView<LoginPresenter> {
+
     void showProgress();
 
     void hideProgress();
@@ -15,13 +14,9 @@ public interface LoginView extends BaseView<LoginPresenter> {
 
     void navigateToHome();
 
-    void setGoogleApiClient();
-
     void loginWithGoogle();
 
     void loginWithEmail();
-
-    OptionalPendingResult<GoogleSignInResult> getOptionalPendingResult();
 
     void showLoginMessage();
 
